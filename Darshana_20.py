5dded2d210f82ca6d93a3ebb2bcc652834a0c0a5
Dhonrao <darshanahonrao193@gmail.com>
2025-07-21 21:43:23 +0530

    Darshana is pushing file..

--- Darshana_20.py
+++ Darshana_20.py
@@ -4,3 +4,13 @@ for i in range(1,16):
   num = num + i
 
 print(num)
+
+
+for i in range(0,5):
+  for j in range(i+1):
+    print("*",end=" ")
+  print()
+for k in range(5,1,-1):
+  for l in range(k-1):
+    print("*",end=" ")
+  print()
